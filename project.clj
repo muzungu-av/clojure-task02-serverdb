c64b4362dccf0a87ca66fe85a5b94cdb888681c7
Arykin Valera <muzungu.av@gmail.com>
2020-12-17 21:00:07 +0300

    run test

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://clojurecourse.by"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.csv "0.1.2"]]
   :aot [task02.Database task02.core]
   :main task02.core)
